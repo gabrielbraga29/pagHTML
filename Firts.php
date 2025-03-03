1868f73516ddee21e0e22f5eea8daf128e2fe817
gabrielbraga29 <gabrielbragasousa24@gmail.com>
2025-03-03 16:31:41 -0300

    Add initial file Firts.php

---
+++ Firts.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+
+    </style>
+</head>
+<body>
+    <h1 style="color: red;">OLÁ MUNDO!! </h1>
+
+    <p>Esse é meu primeiro HTML!</p>
+    
+</body>
+</html>
